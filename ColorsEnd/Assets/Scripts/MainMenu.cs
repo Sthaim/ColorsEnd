4614c1bac8ea306b27916951f71c093be1d6b300
Nethys79 <95236064+Nethys79@users.noreply.github.com>
2022-05-14 18:43:03 +0200

    up

---
+++ ColorsEnd/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+    public GameObject mainMenu;
+    public GameObject optionMenu;
+    public GameObject creditMenu;
+
+    private void Start()
+    {
+        mainMenu.SetActive(true);
+        optionMenu.SetActive(false);
+        creditMenu.SetActive(false);
+    }
+
+    // Start is called before the first frame update
+    public void play()
+    {
+        
+    }
+
+    public void option()
+    {
+        mainMenu.SetActive(false);
+        optionMenu.SetActive(true);
+        creditMenu.SetActive(false);
+    }
+
+    public void credit()
+    {
+        mainMenu.SetActive(false);
+        optionMenu.SetActive(false);
+        creditMenu.SetActive(true);
+    }
+
+    public void back()
+    {
+        mainMenu.SetActive(true);
+        optionMenu.SetActive(false);
+        creditMenu.SetActive(false);
+    }
+
+    public void OnApplicationQuit()
+    {
+        
+    }
+}
