c650ac5bf3c3ff8860f083ffe8226dd82375ed98
DingerM GD2-2 <32112541+Sthaim@users.noreply.github.com>
2022-05-14 17:45:18 +0200

    Bordel

--- Assets/Scripts/Movement/Follower.cs
+++ Assets/Scripts/Movement/Follower.cs
@@ -1,31 +1,56 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.AI;
+
+
 
 [RequireComponent(typeof(NavMeshAgent))]
 
 public class Follower : MonoBehaviour
 {
+    private FormationBase _formation;
+
+    public FormationBase Formation
+    {
+        get
+        {
+            if (_formation == null) _formation = GetComponent<FormationBase>();
+            return _formation;
+        }
+        set => _formation = value;
+    }
+
+    [SerializeField] private GameObject _unitPrefab;
+    [SerializeField] private float _unitSpeed = 2;
+
+    private readonly List<GameObject> _spawnedUnits = new List<GameObject>();
+    private List<Vector3> _points = new List<Vector3>();
+    private Transform _parent;
+
     private ClickMovement m_leaderToFollow;
 
     private NavMeshAgent m_agent;
 
-    private int m_addedArrive;
-
     // Start is called before the first frame update
     void Start()
     {
-        m_addedArrive = 0;
         m_agent = GetComponent<NavMeshAgent>();
+    }
+
+    private void Awake()
+    {
+        _parent = new GameObject("Unit Parent").transform;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (m_leaderToFollow)
+        SetFormation();
+        /*if (m_leaderToFollow)
         {
-            if (Vector3.Distance(m_leaderToFollow.GetLeaderPosition(), transform.position) > (2 + (m_leaderToFollow.m_nombreArrive * 0.3)))
+            if (Vector3.Distance(m_leaderToFollow.GetLeaderPosition(), transform.position) > (3 + (m_leaderToFollow.m_nombreArrive * 0.7)))
             {
                 m_agent.SetDestination(m_leaderToFollow.GetLeaderPosition());
                 if (m_addedArrive != 2)
@@ -43,16 +68,51 @@ public class Follower : MonoBehaviour
                     m_addedArrive = 1;
                 }
             }
-        }
-        if (m_leaderToFollow)
-        {
-            
-        }
-        
+        }*/
     }
 
     public void SetLeader(ClickMovement p_leader)
     {
         m_leaderToFollow = p_leader;
     }
+
+    private void SetFormation()
+    {
+        _points = Formation.EvaluatePoints().ToList();
+
+        /*if (_points.Count > _spawnedUnits.Count)
+        {
+            var remainingPoints = _points.Skip(_spawnedUnits.Count);
+            Spawn(remainingPoints);
+        }
+        else if (_points.Count < _spawnedUnits.Count)
+        {
+            Kill(_spawnedUnits.Count - _points.Count);
+        }*/
+
+        for (var i = 0; i < _spawnedUnits.Count; i++)
+        {
+            _spawnedUnits[i].transform.position = Vector3.MoveTowards(_spawnedUnits[i].transform.position, transform.position + _points[i], _unitSpeed * Time.deltaTime);
+        }
+    }
+
+    public void Spawn(IEnumerable<Vector3> points)
+    {
+        foreach (var pos in points)
+        {
+            var unit = Instantiate(_unitPrefab, transform.position + pos, Quaternion.identity);
+            _spawnedUnits.Add(unit);
+        }
+    }
+
+    private void Kill(int num)
+    {
+        for (var i = 0; i < num; i++)
+        {
+            var unit = _spawnedUnits.Last();
+            _spawnedUnits.Remove(unit);
+            Destroy(unit.gameObject);
+        }
+    }
+}
 }
