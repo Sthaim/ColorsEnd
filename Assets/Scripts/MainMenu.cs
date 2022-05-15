03bf48168a2572f923d56c0ebc3c19e5f154a286
nanutra <nicolas.mendiboure@gmail.com>
2022-05-15 06:48:31 +0200

    ReadyForBuild

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
@@ -21,7 +22,7 @@ public class MainMenu : MonoBehaviour
     // Start is called before the first frame update
     public void play()
     {
-        
+        SceneManager.LoadScene("TestingGround");
     }
 
     public void option()
